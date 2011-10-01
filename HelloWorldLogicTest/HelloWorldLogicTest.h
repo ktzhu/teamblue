23db41209dc5643067a57a00e429f36e5fda43f4
Thanapon Noraset <flcl.tvboy@gmail.com>
2011-10-01 13:38:42 -0500

    Add iOS Logic Test target (native Cocoa Touch Unit Test)

---
+++ HelloWorldLogicTest/HelloWorldLogicTest.h
@@ -0,0 +1,13 @@
+//
+//  HelloWorldLogicTest.h
+//  HelloWorldLogicTest
+//
+//  Created by Thanapon Noraset on 10/1/11.
+//  Copyright 2011 Northwestern University. All rights reserved.
+//
+
+#import <SenTestingKit/SenTestingKit.h>
+
+@interface HelloWorldLogicTest : SenTestCase
+
+@end
---
+++ HelloWorldLogicTest/HelloWorldLogicTest.m
@@ -0,0 +1,32 @@
+//
+//  HelloWorldLogicTest.m
+//  HelloWorldLogicTest
+//
+//  Created by Thanapon Noraset on 10/1/11.
+//  Copyright 2011 Northwestern University. All rights reserved.
+//
+
+#import "HelloWorldLogicTest.h"
+
+@implementation HelloWorldLogicTest
+
+- (void)setUp
+{
+    [super setUp];
+    
+    // Set-up code here.
+}
+
+- (void)tearDown
+{
+    // Tear-down code here.
+    
+    [super tearDown];
+}
+
+- (void)testExample
+{
+    STFail(@"Unit tests are not implemented yet in HelloWorldLogicTest");
+}
+
+@end
