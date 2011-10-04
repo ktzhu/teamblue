b428cf62b2f585eba99f4680b2d86ff4bf9cdd40
Shawn O'Banion <shawn.obanion@gmail.com>
2011-10-04 10:47:51 -0500

    Simple change to break a test.

--- www/src-test/greeting-tests.js
+++ www/src-test/greeting-tests.js
@@ -2,5 +2,5 @@ GreeterTest = TestCase("GreeterTest");
 
 GreeterTest.prototype.testGreet = function() {
   var greeter = new myapp.Greeter();
-  assertEquals("Hello World!", greeter.greet("World"));
+  assertEquals("Hello World!!!", greeter.greet("World"));
 };
