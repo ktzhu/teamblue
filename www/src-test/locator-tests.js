211f2baea75415063b75b3e487db50368b2acf90
Katie Zhu <kzhu91@gmail.com>
2011-10-07 14:48:27 -0500

    Added simple geolocation unit test.

---
+++ www/src-test/locator-tests.js
@@ -0,0 +1,9 @@
+LocationTest = TestCase("LocationTest");
+
+LocationTest.prototype.testChicago = function () {
+	var loc = new myapp.Locator();
+	// Coordinates for Chicago:
+	var latitude = 41.881944;
+	var longitude = -87.627778;
+	assertEquals("Found you! " + latitude + ", " + longitude, loc.locate(latitude, longitude));
+}
